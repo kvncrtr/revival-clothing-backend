d513de7ff19464855079a424b582e66aa19e0b17
Kevin <console.logkevin@gmail.com>
2026-04-26 23:00:50 -0400

    feature(init): Dynamic route
    - The dynamic components in routes are strings by default but can also
    be of different types
    - The path type is a special string type that can
    include forward slashes, unlike the string type.
    - Learn Once the server starts up, it goes into a loop that accepts
    requests and services them. This loop continues until you stop the
    application by pressing Ctrl+C.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get('/')
-def home():
-    return {'message': 'FastAPI is running'}
+@app.get('/drops/{num}', response_class=HTMLResponse)
+def drop(num: int):
+    return f'<h1>Drop collection you subscribed to: {num}</h1>'
